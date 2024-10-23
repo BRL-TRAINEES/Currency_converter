369269fe8c6c8905a9640e6e74498072fad266eb
Dikshant <dikshantahalawat2005@gmail.com>
2024-10-23 10:39:50 +0530

    hide apikey

--- lib/services.dart
+++ lib/services.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:currency_converter/user_model.dart';
-
+import 'config/config.dart';
 class CurrencyService {
   Future<CurrencyModel> fetchCurrencyRates() async {
     print("Fetching currency rates...");
     final response = await http.get(
-      Uri.parse(
-          'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_SnQMgkmAc88RgVEh2pI8yAdmuogqk6ZgEojD4Oyc'),
+      Uri.parse(('https://${Config.hostName}/v1/latest?apikey=${Config.apiKey}'),),
+          
     );
     print('Response status: ${response.statusCode}');
     print('Response: ${response.body}');
